06140e4cc684a10f2a4156373f6f63687147b4f5
@Konstantine <57166789+mind-protector@users.noreply.github.com>
2019-11-10 13:33:29 +0500

    Add files via upload

---
+++ deinstall.py
@@ -0,0 +1,11 @@
+from getpass import getuser
+import os
+
+os.remove("$POST_WIN32.pyw")
+os.remove("winlock.pyw")
+
+os.chdir(f"C:/Users/{getuser()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/")
+os.remove("SystemPlugins.bat")
+
+from getpass import getuser
+print(f"C:/Users/{getuser()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/")
